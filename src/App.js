3203f29879a0b989b0017405e3a3861ace48eb00
22jz0134 <22jz0134@jec.ac.jp>
2025-01-24 10:13:09 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './pages/top';
 import ItemList from './components/ItemList';
 import ListingDetail from './components/ListingDetail';
